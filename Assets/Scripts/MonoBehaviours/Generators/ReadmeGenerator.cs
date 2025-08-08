be55924d660b9e8f58e5cae1009d34dac74f37ca
Mathias Johansson <mathias@coherence.io>
2025-08-09 00:35:40 +0200

    doc: README animations

--- Assets/Scripts/MonoBehaviours/Generators/ReadmeGenerator.cs
+++ Assets/Scripts/MonoBehaviours/Generators/ReadmeGenerator.cs
@@ -1,13 +1,29 @@
-﻿using Unity.Collections;
+﻿using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class ReadmeGenerator : Generator
 {
+    public GameObject WalkerPrefab;
+    public bool GenerateWalkers = true;
+
     public float Delay = 0.3f;
     public int MinValue = -3;
     public int MaxValue = 15;
 
     private float lastTime;
+    private readonly Vector3[] walkerSpawnPositions =
+    {
+        new(1, 0, 0),
+        new(0, 0, 5),
+        new(6, 0, 1),
+        new(5, 0, 6),
+    };
+    private readonly List<GameObject> walkers = new();
+    private FlowFieldConfig config;
+
+    private void Awake() => config = GetComponent<FlowFieldConfig>();
 
     public override void Generate(NativeArray<float> inputField, int width, int height, string seed)
     {
@@ -36,7 +52,53 @@ public class ReadmeGenerator : Generator
         }
 
         lastTime = Time.time;
-        var config = GetComponent<FlowFieldConfig>();
+
+        if (GenerateWalkers)
+        {
+            UpdateWalkers();
+        } else
+        {
+            UpdateBakeIterations();
+        }
+    }
+
+    private void UpdateWalkers()
+    {
+        if (!config.FlowField.NextIndices.IsCreated)
+        {
+            return;
+        }
+
+        if (walkers.Count < walkerSpawnPositions.Length)
+        {
+            var go = Instantiate(WalkerPrefab, walkerSpawnPositions[walkers.Count], Quaternion.identity);
+            go.name = "Walker";
+            walkers.Add(go);
+        }
+
+        for (var index = 0; index < walkers.Count; index++)
+        {
+            var walker = walkers[index];
+            var pos = new float2(walker.transform.position.x, walker.transform.position.z);
+            var cell = (int2)math.round(pos);
+            var cellIndex = cell.x + cell.y * config.FlowField.Width;
+            var nextIndex = config.FlowField.NextIndices[cellIndex];
+            if (nextIndex == cellIndex)
+            {
+                walker.transform.position = walkerSpawnPositions[index];
+            }
+            else
+            {
+                cellIndex = config.FlowField.NextIndices[cellIndex];
+                cell.x = cellIndex % config.FlowField.Width;
+                cell.y = cellIndex / config.FlowField.Width;
+                walker.transform.position = new Vector3(cell.x, 0, cell.y);
+            }
+        }
+    }
+
+    private void UpdateBakeIterations()
+    {
         var value = config.BakeOptions.Iterations;
         value++;
         var cycled = ((value - MinValue) % (MaxValue - MinValue) + (MaxValue - MinValue)) % (MaxValue - MinValue) + MinValue;
